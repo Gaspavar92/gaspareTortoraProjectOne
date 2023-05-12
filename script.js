5e3b72acb9d5702476b7494cc28bf3c55328409d
Gaspare Tortora <g.tortora2@gmail.com>
2023-05-11 22:02:07 -0400

    Improved mobile navigation menu

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Selecting DOM elements to create mobile nav menu
 
-const burgerMenu = document.querySelector('.hamburger-menu');
+const burgerMenu = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 // Defining function to open mobile menu
 
 const openMenu = () => {
     mobileMenu.classList.toggle('show');
+    burgerMenu.classList.toggle('is-active');
 }
 burgerMenu.addEventListener('click', openMenu)
 
