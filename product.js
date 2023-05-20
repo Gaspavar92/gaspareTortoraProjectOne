e80eebedec992a6972ee28200fa86ea56ac592a1
Gaspare Tortora <g.tortora2@gmail.com>
2023-05-19 21:42:53 -0400

    Styled product preview

--- product.js
+++ product.js
@@ -6,6 +6,10 @@ const productTitle = document.querySelector('.product-title');
 const productDescription = document.querySelector('.product-description');
 const productPrice = document.querySelector('.product-price');
 const productImage = document.querySelector('.product-image');
+const quantity = document.querySelector('.quantity');
+const addBtn = document.querySelector('.add-btn');
+const rmvBtn = document.querySelector('.remove-btn');
+const addToCart = document.querySelector('.add-to-cart');
 
 const closeButton = document.createElement('button');
 closeButton.classList.add('close-button');
@@ -27,7 +31,34 @@ products.forEach(item => item.addEventListener('click', (event) => {
 
 closeButton.addEventListener('click', (event) => {
     event.stopPropagation();
-    event.target.parentElement.parentElement.classList.remove('open-product');
-    event.target.parentElement.parentElement.classList.add('hidden');
+    productDetails.classList.remove('open-product');
+    productDetails.classList.add('hidden');
     event.target.parentElement.parentElement.removeChild(closeButton);
+    quantity.value = 0;
+})
+
+addBtn.addEventListener('click', () => {
+    quantity.value++;
+})
+rmvBtn.addEventListener('click', () => {
+    quantity.value > 0 ? quantity.value-- : quantity.value = 0;
+})
+
+// Updating cart quantity
+
+const cartMessage = document.querySelector('.cart-message');
+
+let total = 0;
+
+const p = document.createElement('p');
+p.innerHTML = `You have ${total} item(s) in your cart`;
+p.setAttribute('style', 'color:white; margin-left: 20px; font-size: 2rem;')
+cartMessage.append(p);
+
+addToCart.addEventListener('click', () => {
+    total = total + parseInt(quantity.value);
+    p.innerHTML = `You have ${total} item(s) in your cart`;
+    productDetails.classList.remove('open-product');
+    productDetails.classList.add('hidden');
+    quantity.value = 0;
 })
